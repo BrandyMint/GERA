f375e513918b1a0e0a60143b07c8d5af7ebec352
Danil Pismenny <danil@brandymint.ru>
2022-04-20 10:07:06 +0300

    m

--- app/decorators/gera/external_rate_decorator.rb
+++ app/decorators/gera/external_rate_decorator.rb
@@ -2,5 +2,10 @@
 
 module Gera
   class ExternalRateDecorator < ApplicationDecorator
+    delegate_all
+
+    def rate_value
+      h.rate_humanized_description(object.rate_value, cur_from, cur_to)
+    end
   end
 end
